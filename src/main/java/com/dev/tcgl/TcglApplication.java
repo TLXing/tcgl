e6569f95a3e40aed9c3400837042a98050064033
LingXing Tan <839472561@qq.com>
2019-03-12 18:29:07 +0800

    first commit

---
+++ src/main/java/com/dev/tcgl/TcglApplication.java
@@ -0,0 +1,13 @@
+package com.dev.tcgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcglApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcglApplication.class, args);
+    }
+
+}
